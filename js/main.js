33be3e2892abdf8af1788bc917cb19e598d39153
shdwgdx <110768105+shdwgdx@users.noreply.github.com>
2023-06-10 21:59:18 +0300

    Add files via upload
    2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 document.querySelector(".second-button").addEventListener("click", function () {
   document.querySelector(".animated-icon2").classList.toggle("open");
 });
+
+const button = document.querySelector(".navbar-toggler");
+const content = document.querySelector(".navbar-brand");
+const navigation = document.querySelector(".navigation");
+// button.addEventListener("click", function () {
+//   content.style.display =
+//     content.style.display == "none" ? "inline-block" : "none";
+// });
+const nav = document.querySelector(".navigation");
+const startScroll = window.pageYOffset;
+window.addEventListener("scroll", () => {
+  if (window.innerWidth < 992) {
+    if (window.pageYOffset > startScroll) {
+      nav.style.background = "#00aaff";
+      content.style.visibility = "hidden";
+      navigation.style.position = "fixed";
+    } else {
+      nav.style.background = "";
+      content.style.visibility = "visible";
+      navigation.style.position = "static";
+    }
+  }
+});
